ccb96f38cf7dc71739c76ebabcd4f3c12ee5194d
saurabhambar <saurabh.ambar2017@gmail.com>
2019-10-09 21:52:47 +0530

    marco_c++

---
+++ 1206_3_marcos.cpp
@@ -0,0 +1,17 @@
+include<iostream>
+
+using namespace std
+
+#define SQRT(x) (x * x)
+
+int main(){
+    int a, b = 3;
+    a = SQRT(b++);
+    cout<< a << endl << b;
+    return 0;
+}
+
+//output:   12
+//          5
+//(b++ * b++) = 3 * 4
+// at last value of b becomes 5
